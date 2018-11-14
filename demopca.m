9a264c79e71aba44a644518efb6f4feb6be18384
Unknown <arley-jams@hotmail.com>
2018-11-14 11:49:30 -0500

    Comparison of covariances

--- demopca.m
+++ demopca.m
@@ -18,7 +18,7 @@ F1=F-(kron(f,ones(1,n)));
 Q=rand(shots,100);
 % designed matrix
 transmittance = 1/1;
-Q_tilde = designbinary_pca(shots,F1*F1'./n,transmittance);
+[Q_tilde ,Sigmarest]= designbinary_pca(shots,F1*F1'./n,transmittance);
 
 % sense and reconstruct using Pseudo-Inverse
 
@@ -34,4 +34,10 @@ plot(1:l,F(:,pix),'red',1:l,Fr(:,pix),'blue',1:l,Fd(:,pix),'black'),
 legend('Ground-truth','Random','Designed')
 
 fprintf('||F-Fd||_2=%f, ||F-Fr||_2=%f, SNR_Designed=%f, SNR_Random=%f\n',norm(F-Fd),norm(F-Fr),snr(F,F-Fd),snr(F,F-Fr));
+
+%% comparison of covariance
+
+showSigmas=[F1*F1'./n,zeros(l,2)-1,Sigmarest,zeros(l,2)-1,F1*F1'./n-Sigmarest];
+
+imagesc(showSigmas),xlabel('Original Covariance, Information Deflated, Residual')
 
