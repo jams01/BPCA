edfdf87283ac9b06223ca86f0ddbc7ce19a37d8d
Unknown <arley-jams@hotmail.com>
2018-11-14 14:40:11 -0500

    update documentation

--- demopca.m
+++ demopca.m
@@ -17,13 +17,13 @@ F1=F-(kron(f,ones(1,n)));
 % random sensing matrix
 Q=rand(shots,100);
 % designed matrix
-transmittance = 1/1;
+transmittance = 1/1; %maximun possible transmittance
 [Q_tilde ,Sigmarest]= designbinary_pca(shots,F1*F1'./n,transmittance);
 
 % sense and reconstruct using Pseudo-Inverse
 
-Yr=Q*F;
-Yd=Q_tilde*F;
+Yr=Q*F; %random measurements
+Yd=Q_tilde*F; %designed measurments
 
 Fr=pinv(Q)*Yr;
 Fd=pinv(Q_tilde)*Yd;
@@ -39,5 +39,5 @@ fprintf('||F-Fd||_2=%f, ||F-Fr||_2=%f, SNR_Designed=%f, SNR_Random=%f\n',norm(F-
 
 showSigmas=[F1*F1'./n,zeros(l,2)-1,Sigmarest,zeros(l,2)-1,F1*F1'./n-Sigmarest];
 
-imagesc(showSigmas),xlabel('Original Covariance, Information Deflated, Residual')
+figure,imagesc(showSigmas),xlabel('Original Covariance, Information Deflated, Residual')
 
